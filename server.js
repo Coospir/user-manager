0e4464229e072d4f394685093b0bcb60b15ecb85
Coospir <eugene.starodubov@gmail.com>
2021-02-24 17:00:15 +0300

    added routes (p1)

--- server.js
+++ server.js
@@ -33,4 +33,8 @@ app.get('/add-user', (req, res) => {
     res.render('add_user')
 })
 
+app.get('/update-user', (req, res) => {
+    res.render('update_user')
+})
+
 app.listen(3000, () => console.log(`Server is running.. http://localhost:${PORT}`))
