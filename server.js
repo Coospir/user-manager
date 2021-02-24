cbeb536d2709dff93ea44e09283d436f73572ea8
Coospir <eugene.starodubov@gmail.com>
2021-02-24 15:40:19 +0300

    updated styles

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const env = require('dotenv')
-const app = express()
-const PORT = process.env.PORT || 8080
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const path = require('path')
+
+const app = express()
+
 
 env.config( {path:'config.env'} )
+const PORT = process.env.PORT || 8080
 
 app.use(morgan('tiny'))
 
@@ -14,11 +17,16 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // Set view engine
 app.set('view engine', 'ejs')
+//app.set('views', path.resolve(__dirname, "views/ejs"))
+
+// Load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
 
 
 app.get('/', (req, res) => {
-    res.send('Users Manager')
+    res.render('index')
 })
 
 app.listen(3000, () => console.log(`Server is running.. http://localhost:${PORT}`))
